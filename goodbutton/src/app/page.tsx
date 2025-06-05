cc0a6948e02d58bee2288b15103834ab6ce3934c
seikin <haru.mo.1113@gmail.com>
2025-06-05 15:12:18 +0900

    リセットbutton作成

--- goodbutton/src/app/page.tsx
+++ goodbutton/src/app/page.tsx
@@ -2,18 +2,28 @@
 import { Fragment, useState } from "react";
 
 export default function Home() {
+  const [count,setCount]=useState(0);
+
   return (
     <div className="flex justify-center items-center h-screen">
-      <LikeButton/>
+      <LikeButton count={count} setCount={setCount}/>
+      <CleanButton setCount={setCount}/>
     </div>
   );
 }
 
-function LikeButton() {
-  const [count,setCount]=useState(0);
+
+function LikeButton({count,setCount}:{ count: number; setCount: (n: number) => void }) {
   const handleClick=()=>{
     setCount(count+1);
   }
 
   return <span className="likeButton" onClick={handleClick}>♥ {count}</span>;
+}
+
+function CleanButton({setCount}:{setCount:(n:number)=>void}){
+  const handleCleanClick=()=>{
+    setCount(0);
+  }
+  return <span className="bg-sky-500 text-white rounded-lg cursor-pointer mx-5 px-3" onClick={handleCleanClick}>リセット</span>
 }
